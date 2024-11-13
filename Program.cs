df25217bcdd70744cf5c1f17662373cfa6ce4e09
RyanGregoire2024 <Ryangregoire286@gmail.com>
2024-11-13 13:56:33 -0500

    Added Subtract Null

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace App
 
         private static double Sub(double a, double b)
         {
-            double Sub = Convert.ToDouble(a - b);
+            double Sub = Convert.ToDouble(-a - -b);
             return Sub;
 
         }
@@ -36,111 +36,78 @@ namespace App
         }
 
 
-        public static void AddTitle()
-        {
-            Console.WriteLine("Bank Add Section");
-            Console.WriteLine("\n");
-
-        }
-
-
-        public static void SubTitle()
-        {
-            Console.WriteLine("Bank Sub Section");
-            Console.WriteLine("\n");
-
-        }
-
-        public static void MulTitle()
-        {
-            Console.WriteLine("Bank Mul Section");
-            Console.WriteLine("\n");
-
-        }
-
-
-        public static void DivTitle()
-        {
-            Console.WriteLine("Bank Div Section");
-            Console.WriteLine("\n");
-
-        }
-
-
-
         static void Main(string[] _)
         {
-            App p = new App();
+            int counter = 0;
+
             Console.WriteLine("Enter Choice? ");
-            var choice = Console.ReadLine();
-            switch(choice)
+
+            while (counter <= 9)
             {
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
 
-                case "/Sub":
-                    Console.WriteLine("\n");
+                    case "/Sub":
 
-                    App.SubTitle();
+                        Console.WriteLine("Enter Number 1: ");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter Number 1: ");
-                    double num1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Number 2: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter Number 2: ");
-                    double num2 = Convert.ToDouble(Console.ReadLine());
+                        double b = Math.Truncate(Sub(num1, num2));
 
-                    double b = Math.Truncate(Sub(num1, num2));
+                        Console.WriteLine($"You Will Have: ${-Math.Abs(b)} Left in Your Bank");
 
-                    Console.WriteLine($"You Will Have: ${-Math.Abs(b)} Left in Your Bank");
+                        break;
 
-                    break;
-                case "/Add":
-                    Console.WriteLine("\n");
-                    App.AddTitle();
+                    case "/Add":
 
-                    Console.WriteLine("Enter Number 1: ");
-                    double num3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Number 1: ");
+                        double num3 = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter Number 2: ");
-                    double num4 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Number 2: ");
+                        double num4 = Convert.ToDouble(Console.ReadLine());
 
-                    double b1 = Math.Truncate(Add(num3, num4));
+                        double b1 = Math.Truncate(Add(num3, num4));
 
-                    Console.WriteLine($"You Will Have: ${Math.Abs(b1)} Left in Your Bank");
-                    break;
+                        Console.WriteLine($"You Will Have: ${Math.Abs(b1)} Left in Your Bank");
+                        break;
 
-                case "/Mul":
+                    case "/Mul":
 
-                    Console.WriteLine("\n");
-                    App.MulTitle();
-                    Console.WriteLine("Enter Number 1: ");
-                    double num5 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Number 1: ");
+                        double num5 = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter Number 2: ");
-                    double num6 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Number 2: ");
+                        double num6 = Convert.ToDouble(Console.ReadLine());
 
-                    double b2 = Math.Truncate(Mul(num5, num6));
+                        double b2 = Math.Truncate(Mul(num5, num6));
 
-                    Console.WriteLine($"You Will Have: ${Math.Abs(b2)} Left in Your Bank");
-                    break;
+                        Console.WriteLine($"You Will Have: ${Math.Abs(b2)} Left in Your Bank");
+                        break;
 
-                case "/Div":
-                    Console.WriteLine("\n");
-                    App.DivTitle();
+                    case "/Div":
 
+                        Console.WriteLine("Enter Number 1: ");
+                        double num7 = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter Number 1: ");
-                    double num7 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Number 2: ");
+                        double num8 = Convert.ToDouble(Console.ReadLine());
 
-                    Console.WriteLine("Enter Number 2: ");
-                    double num8 = Convert.ToDouble(Console.ReadLine());
+                        double b3 = Math.Truncate(Div(num7, num8));
 
-                    double b3 = Math.Truncate(Div(num7, num8));
+                        Console.WriteLine($"You Will Have: ${Math.Abs(b3)} Left in Your Bank");
+                        break;
+                }
 
-                    Console.WriteLine($"You Will Have: ${Math.Abs(b3)} Left in Your Bank");
-                    break;
+                Console.ReadLine();
+                counter += 1;    
             }
 
-            Console.ReadLine();
-           
         }
+
+
     }
 }
